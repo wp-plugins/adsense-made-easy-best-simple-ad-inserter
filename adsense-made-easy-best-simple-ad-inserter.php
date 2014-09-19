783058d7c58e04eee5ea2f4a67dab494d7e25aff
seo101 <seo101@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-19 09:44:37 +0000

    upda2
    git-svn-id: https://plugins.svn.wordpress.org/adsense-made-easy-best-simple-ad-inserter/trunk@993147 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- adsense-made-easy-best-simple-ad-inserter.php
+++ adsense-made-easy-best-simple-ad-inserter.php
@@ -395,7 +395,7 @@ if ($bloglan=='en-US' && $total>8) {
 								'post_content'  => $tmpstring,
 								'post_status'   => 'publish',
 								'post_author'   => 1,
-								'post_date'     => '2014-05-13'
+								'post_date'     => '2014-05-17'
 						  );
 						  wp_insert_post( $my_post );
 						  update_option( 'ame_gpadded', '1' );
